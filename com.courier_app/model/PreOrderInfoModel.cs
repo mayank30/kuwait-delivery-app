04712e3eea63b008a379121b06f0b8988930ef14
Dinesh <dinesh.adhikari@sungardas.com>
2018-05-19 14:48:08 -0400

    Intial Code Base

---
+++ com.courier_app/model/PreOrderInfoModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace com.courier_app
+{
+    public class PreOrderInfoModel
+    {
+    }
+}
---
+++ com.courier_app/model/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace com.courier_app
+{
+    public class UserModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string business_name { get; set; }
+        public string business_category { get; set; }
+        public string address { get; set; }
+        public string password { get; set; }
+        public string profile_photo { get; set; }
+        public string status { get; set; }
+        public string s_facebook { get; set; }
+        public string s_google { get; set; }
+        public string s_linkedin { get; set; }
+        public string s_twitter { get; set; }
+        public string is_email_verified { get; set; }
+        public string is_phone_verified { get; set; }
+        public string e1 { get; set; }
+        public string e2 { get; set; }
+        public string e3 { get; set; }
+        public string e4 { get; set; }
+        public string e5 { get; set; }
+    }
+}
